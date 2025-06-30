e50a222c38cda02f221b7d4803f4b6558aa559d9
Shashank B <shashank.b@codecraft.co.in>
2025-06-30 12:08:21 +0530

    Completed with using traditional input output

---
+++ src/investment-results.ts
@@ -0,0 +1,29 @@
+// Use the below code as a help
+// e.g., integrate it into a service or component
+// You may need to tweak it, depending on where and how you use it
+
+import { InvestmentInput } from "./app/investment-input.model";
+
+function calculateInvestmentResults(data:InvestmentInput) {
+  const { initialInvestment, annualInvestment, expectedReturn, duration } = data;
+  const annualData = [];
+  let investmentValue = initialInvestment;
+
+  for (let i = 0; i < duration; i++) {
+    const year = i + 1;
+    const interestEarnedInYear = investmentValue * (expectedReturn / 100);
+    investmentValue += interestEarnedInYear + annualInvestment;
+    const totalInterest =
+      investmentValue - annualInvestment * year - initialInvestment;
+    annualData.push({
+      year: year,
+      interest: interestEarnedInYear,
+      valueEndOfYear: investmentValue,
+      annualInvestment: annualInvestment,
+      totalInterest: totalInterest,
+      totalAmountInvested: initialInvestment + annualInvestment * year,
+    });
+  }
+
+  return annualData;
+}
